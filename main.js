57db1d0c65b22d58e4c039cb8c249cea8e2bc0a0
stefanB <stefanbolcs@gmail.com>
2020-05-07 21:09:23 +0200

    using component in component

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+Vue.component('task-list',{
+    template:'<div><task v-for="task in tasks">{{task.task}}</task> </div>',
+
+    data(){
+        return {
+            tasks:[
+                {task:'Go to store', complete:true},
+                {task:'Go to the email', complete:false},
+                {task:'Go to the farm', complete:true}
+                
+            ]
+        }
+    }
+})
+
+
+
 Vue.component('task',{
-template:'<li><slot></slot></li>'
+template:'<li><slot></slot></li>',
+
+data(){
+    return {
+        message:'Foobar'
+    }
+}
 });
 
 new Vue({
