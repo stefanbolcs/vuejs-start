fd82a606cc404f12146507d7cb191222a5bf0291
stefanB <stefanbolcs@gmail.com>
2020-05-08 13:30:39 +0200

    using nested component [not working] but logic is ok

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
-Vue.component('task-list',{
-    template:'<div><task v-for="task in tasks">{{task.task}}</task> </div>',
-
-    data(){
-        return {
-            tasks:[
-                {task:'Go to store', complete:true},
-                {task:'Go to the email', complete:false},
-                {task:'Go to the farm', complete:true}
-                
-            ]
-        }
-    }
-})
-
-
-
-Vue.component('task',{
-template:'<li><slot></slot></li>',
-
-data(){
-    return {
-        message:'Foobar'
-    }
-}
-});
-
-new Vue({
-    el: '#root'
+Vue.component('message',{
+    props: ['title','body'],
+    template: ' <article class="message">  <div class="message-header"> {{title}}   </div><div class="message-body">{{body}}</div></article>'
 });
 
