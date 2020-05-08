826cd2559d6a2e0b446a1dd4cc5fe95fc81eff5f
stefanB <stefanbolcs@gmail.com>
2020-05-08 17:40:09 +0200

    created app with vue cli  and  vuex to try state management

---
+++ hello-world/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/> -->
+    
+    
+    {{this.$store.state.test}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name:'app',
+  data(){
+    return {
+      msg: 'welcome tod your vue app'
+    }
+  }
+}
+</script>
+
+
