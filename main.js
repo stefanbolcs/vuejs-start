96979d00c5238d0e5f3ede4fcaec0dbffb94b7b2
stefanB <stefanbolcs@gmail.com>
2020-05-08 14:55:21 +0200

    vuex demo [not working]

--- main.js
+++
@@ -1,5 +0,0 @@
-Vue.component('message',{
-    props: ['title','body'],
-    template: ' <article class="message">  <div class="message-header"> {{title}}   </div><div class="message-body">{{body}}</div></article>'
-});
-
